5826650dbd341d102d10fa2d55aa6a129f4ec8e6
kelilipann <lacitaalifa@gmail.com>
2026-09-21 03:46:57 +0700

    Update data.py

--- data.py
+++ data.py
@@ -8,4 +8,5 @@ Original file is located at
 """
 nama_produk = input("Masukkan nama produk: ")
 jumlah_produk = int(input("Masukkan jumlah produk: "))
+satuan_produk = input("Masukkan satuan produk (kg/liter): ")
 # Data
